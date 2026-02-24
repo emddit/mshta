e0275b8dbbac43924d97688bde365ffd490f4d46
Thecoolcoder <79680268+3030years@users.noreply.github.com>
2026-02-24 00:08:02 -0600

    Update install.bat

--- ipxe/install.bat
+++ ipxe/install.bat
@@ -9,15 +9,15 @@ echo format fs=fat32 quick >> X:/part.script
 echo assign letter J >> X:/part.script
 echo create partition primary >> X:/part.script
 echo format fs=ntfs quick >> X:/part.script
-echo assign letter k >> X:/part.script
-echo quit >> X:/part.script
+echo assign letter K >> X:/part.script
+echo exit >> X:/part.script
 echo Running diskpart >> X:\log.txt
 title Formatting disks...
 diskpart /s "X:/part.script" >> X:\log.txt
 title Installing net driver...
 "X:\Program Files\CollabVM\InstallDriver.bat" >> X:\Log.txt
 title Running startnet...
-"X:\Program Files\PENetwork_x64\startnet.exe" >> X:\Log.txt
+start "X:\Program Files\PENetwork_x64\startnet.exe" >> X:\Log.txt
 title Done!
 echo Done >> X:\Log.txt
 exit
