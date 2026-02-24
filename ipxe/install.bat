d26636867368d34f7974061d46f70cadb0ce174d
Thecoolcoder <79680268+3030years@users.noreply.github.com>
2026-02-24 00:33:46 -0600

    Update install.bat

--- ipxe/install.bat
+++ ipxe/install.bat
@@ -1,25 +1,25 @@
 @echo off
 title Preparing...
-echo Starting up > X:\log.txt
-echo select disk 0 >> X:/part.script
-echo clean >> X:/part.script
-echo convert gpt >> X:/part.script
-echo create partition efi size=500 >> X:/part.script
-echo format fs=fat32 quick >> X:/part.script
-echo assign letter J >> X:/part.script
-echo create partition primary >> X:/part.script
-echo format fs=ntfs quick >> X:/part.script
-echo assign letter K >> X:/part.script
-echo exit >> X:/part.script
-echo Running diskpart >> X:\log.txt
+echo Starting up > %HOMEDRIVE%\log.txt
+echo select disk 0 >> %HOMEDRIVE%\part.script
+echo clean >> %HOMEDRIVE%\part.script
+echo convert gpt >> %HOMEDRIVE%\part.script
+echo create partition efi size=500 >> %HOMEDRIVE%\part.script
+echo format fs=fat32 quick >> %HOMEDRIVE%\part.script
+echo assign letter J >> %HOMEDRIVE%\part.script
+echo create partition primary >> %HOMEDRIVE%\part.script
+echo format fs=ntfs quick >> %HOMEDRIVE%\part.script
+echo assign letter K >> %HOMEDRIVE%\part.script
+echo exit >> %HOMEDRIVE%\part.script
+echo Running diskpart >> %HOMEDRIVE%\log.txt
 title Formatting disks...
-diskpart /s "X:/part.script" >> X:\log.txt
+diskpart /s "%HOMEDRIVE%/part.script" %HOMEDRIVE%\log.txt
 title Installing net driver...
-echo Installing NetKVM Driver >> X:\log.txt
-"X:\Program Files\CollabVM\InstallDriver.bat" >> X:\Log.txt
+echo Installing NetKVM Driver %HOMEDRIVE%\log.txt
+"%ProgramFiles%\CollabVM\InstallDriver.bat"
 title Running startnet...
-echo Running startnet.exe >> X:\log.txt
-start "netstart" "X:\Program Files\PENetwork_x64\startnet.exe"
+echo Running startnet.exe >> %HOMEDRIVE%\log.txt
+start "netstart" "%ProgramFiles%\PENetwork\PENetwork.bat"
 title Done!
 echo Done >> X:\Log.txt
 exit
