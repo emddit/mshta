2984a3f135fca61f4ca41a294151df5bbbd7b934
Thecoolcoder <79680268+3030years@users.noreply.github.com>
2026-02-23 18:54:08 -0600

    Update install.bat

--- ipxe/install.bat
+++ ipxe/install.bat
@@ -9,7 +9,7 @@ echo assign letter J >> X:/part.script
 echo create partiton primary >> X:/part.script
 echo format fs=ntfs quick >> X:/part.script
 echo assign letter k >> X:/part.script
-quit >> X:/part.script
+echo quit >> X:/part.script
 echo Running diskpart >> X:\log.txt
 diskpart /s "X:/part.script"
 exit
