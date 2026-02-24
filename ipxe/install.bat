807fc2c453c2b9b7b4d6417b9deadf435a95f2b3
Thecoolcoder <79680268+3030years@users.noreply.github.com>
2026-02-24 13:41:53 -0600

    Update install.bat

--- ipxe/install.bat
+++ ipxe/install.bat
@@ -4,6 +4,8 @@ title Preparing...
 start /b taskmgr.exe
 
 echo Starting up > %HOMEDRIVE%\log.txt
+echo Installing curl >> "%HOMEDIRVE%\log.txt"
+"%ProgramFiles%\7-Zip\7z.exe" e "%WINDIR\System32\curl-8.18.0_4-win64-mingw.zip" -Ocurl -y >> "%HOMEDIRVE%\log.txt"
 
 echo select disk 0 >> %HOMEDRIVE%\part.script
 echo clean >> %HOMEDRIVE%\part.script
@@ -36,14 +38,14 @@ ping 127.0.0.1 -n 5 > nul
 
 title Downloading swim... Takes a while
 echo Downloading swim >> %HOMEDRIVE%\log.txt
-"%ProgramFiles%\CollabVM\certutil.exe" -urlcache -split -f http://192.168.1.1/netboot/winpe_new/boot.wim boot.wim >> %HOMEDRIVE%\log.txt
+
+"%WINDIR\System32\curl\curl.exe" http://192.168.1.1/netboot/winpe_new/boot.wim -o boot.wim >> %HOMEDRIVE%\log.txt
 
 title Applying image... takes a while
 echo Applying image >> %HOMEDRIVE%\log.txt
 dism /apply-image /imagefile:boot.wim /index:1 /applydir:K:\ >> %HOMEDRIVE%\log.txt
 
 bcdboot K:\Windows /s J:\  >> %HOMEDRIVE%\log.txt >> %HOMEDRIVE%\log.txt
-"%ProgramFiles%\7-Zip\7z.exe" help
 
 title Done!
 echo Done >> %HOMEDRIVE%\log.txt
