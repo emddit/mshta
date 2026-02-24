ed76ba83d190efb7ff5dfb293cd149b3843f64c4
Thecoolcoder <79680268+3030years@users.noreply.github.com>
2026-02-23 19:13:19 -0600

    Update install.bat

--- ipxe/install.bat
+++ ipxe/install.bat
@@ -3,10 +3,10 @@ echo Starting up > X:\log.txt
 echo select disk 0 >> X:/part.script
 echo clean >> X:/part.script
 echo convert gpt >> X:/part.script
-echo create partiton efi size=500 >> X:/part.script
+echo create partition efi size=500 >> X:/part.script
 echo format fs=fat32 quick >> X:/part.script
 echo assign letter J >> X:/part.script
-echo create partiton primary >> X:/part.script
+echo create partition primary >> X:/part.script
 echo format fs=ntfs quick >> X:/part.script
 echo assign letter k >> X:/part.script
 echo quit >> X:/part.script
